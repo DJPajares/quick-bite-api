e2da0daf606783b591f7a6f82a9b86d7d24fbc67
DJPajares <dj.pajares@gmail.com>
2025-12-06 00:14:03 +0800

    fix: build error

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import connectDB from './config/database';
 import errorHandler from './middleware/errorHandler';
